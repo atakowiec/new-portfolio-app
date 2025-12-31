6d881148cd7cbe1ba2e4c7bf674de5396518055f
atakowiec <mat.cieszczyk@gmail.com>
2025-12-31 19:04:01 +0100

    Update Kubernetes deployment configurations, workflow, and package dependencies
    - Updated Kubernetes deployment files to support custom secrets and environment variables for Next.js and Strapi.
    - Modified CI/CD workflows for new image naming conventions.
    - Added `mysql2` dependency in Strapi to enable MySQL support.
    - Adjusted package-lock.json and package updates for consistency.
    - Enhanced Strapi URL and token handling.

--- next-app/src/axios/strapi.ts
+++ next-app/src/axios/strapi.ts
@@ -2,13 +2,16 @@
 import {Axios} from 'axios';
 
 const API_URL = process.env.STRAPI_URL || 'http://localhost:1337';
+const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
 
 const instance = new Axios({
   headers: {
-    Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+    Authorization: `Bearer ${STRAPI_TOKEN}`,
   },
 })
 
 export async function fetchStrapi(url: string, locale: "pl" | "en" = "en") {
-  return JSON.parse((await instance.get(`${API_URL}/api/${url}?locale=${locale}`)).data);
+  const result = await instance.get(`${API_URL}/api/${url}?locale=${locale}`);
+
+  return JSON.parse((result).data);
 }
